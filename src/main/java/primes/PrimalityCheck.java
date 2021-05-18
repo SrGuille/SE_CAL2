bbaa5b07f8a4e230a1c28350bf65dcc89a97e41f
Antonia <73851646+vanillepie@users.noreply.github.com>
2021-05-18 11:41:34 +0200

    Slightly changed functionality

--- src/main/java/primes/PrimalityCheck.java
+++ src/main/java/primes/PrimalityCheck.java
@@ -5,45 +5,46 @@
  */
 package primes;
 
-
 /**
  *
  * @author guial
  */
 public class PrimalityCheck {
-    
+
     public static boolean isPrime(int n) throws Exception {
-        boolean prime = true; //we assume innitially it is prime
-        if (n<2) // it doesn't make sense to input numbers smaller than 2
-        {
+        boolean prime = true; // assume innitially it is prime
+
+        if (n < 0) {   // throw exception if number is negative
             throw new Exception();
-        }
-        else{
-            int tryNum=2;
-            while (tryNum<=Math.sqrt(n) && prime==true){ //anything bigger than sqrt(n) is not necessary
-                
-                if (n%tryNum==0) { //if we can find any divisor, this means it is not prime
-                    prime=false;
+        } else if (n < 2) { // 1 and 0 are considered not primes by convention
+            prime = false;
+        } else {
+            int tryNum = 2;
+            while (tryNum <= Math.sqrt(n) && prime == true) { //anything bigger than sqrt(n) is not necessary
+
+                if (n % tryNum == 0) { //if any divisor can be found means that number is not prime
+                    prime = false;
                 }
                 tryNum++;
             }
         }
         return prime;
     }
-    
+
     public static boolean isPrime(Number number) throws Exception {
         int n = number.generateNumber();
-        boolean prime = true; //we assume innitially it is prime
-        if (n<2) // it doesn't make sense to input numbers smaller than 2
-        {
+        boolean prime = true; // assume innitially it is prime
+
+        if (n < 0) {   // throw exception if number is negative
             throw new Exception();
-        }
-        else{
-            int tryNum=2;
-            while (tryNum<=Math.sqrt(n) && prime==true){ //anything bigger than sqrt(n) is not necessary
-                
-                if (n%tryNum==0) { //if we can find any divisor, this means it is not prime
-                    prime=false;
+        } else if (n < 2) { // 1 and 0 are considered not primes by convention
+            prime = false;
+        } else {
+            int tryNum = 2;
+            while (tryNum <= Math.sqrt(n) && prime == true) { //anything bigger than sqrt(n) is not necessary
+
+                if (n % tryNum == 0) { //if any divisor can be found means that number is not prime
+                    prime = false;
                 }
                 tryNum++;
             }
@@ -51,4 +52,3 @@ public class PrimalityCheck {
         return prime;
     }
 }
-
